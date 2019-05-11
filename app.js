82bdb48e50c4cb9d787e8b978b1f4e3dd5b9a4e0
Jefferson Silva <jeffersonxrhudson@gmail.com>
2019-05-11 01:01:17 -0300

    Delete app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const app = express();
-
-app.listen(process.env.PORT || 3000, function()
-{
-    console.log('Rodando na porta ' + (process.env.PORT || 3000));
-});
