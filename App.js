7443e17d56bc7c50bd094106cab9b0abfed88452
Jefferson Silva <jeffersonxrhudson@gmail.com>
2019-06-01 12:16:14 -0300

    404 pronta

--- App.js
+++ App.js
@@ -28,6 +28,11 @@ app.use(bodyParser.json());
 //configurações de rotas
 app.use('/', require('./Routes/principais.js'));
 
+//middleware
+app.use(function(req, res, next) {
+    res.status(404).render('404.ejs');
+});
+
 //configura a porta do servidor
 app.listen(process.env.PORT || 3000, function()
 {
