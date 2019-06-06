233b95aeb1420fe7c87a94f467d68d0d4f14cf0a
obama <obama@imd.ufrn.br>
2019-06-06 17:24:58 -0300

    Começando página de usuarios

--- Controller/usuarioController.js
+++ Controller/usuarioController.js
@@ -1,4 +1,16 @@
 const pool = require('../Config/db.js');
 const crypt = require('../Config/crypt.js');
 
-//nada por enquanto
+module.exports.usuarios_condominio = function (req, res) {
+    query_string = 'SELECT U.nome, U.email, U.banido, C.nome FROM'
+    query_string = query_string + ' tb_usuario U JOIN tb_condominio_usuario UC ON U.id_usuario = UC.id_usuario JOIN';
+    query_string = query_string + ' tb_condominio C ON UC.id_condominio = C.id_condominio WHERE C.id_condominio IN';
+    query_string = query_string + ' (SELECT AC.id_condominio FROM tb_usuario U2 JOIN tb_administrador_condominio AC ON';
+    query_string = query_string + ' U2.id_usuario=AC.id_usuario WHERE U2.id_usuario=$1)';
+    pool.query(query_string, [req.user.id_usuario], (err, res_bd) => {
+        if (err){
+            //internal server error page
+        }
+        res.render('administrador/usuarios.ejs');
+    });
+}
