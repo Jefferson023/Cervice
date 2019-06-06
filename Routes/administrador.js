7742740f26a3361ef1ad2737396d7115902b56cc
obama <obama@imd.ufrn.br>
2019-06-06 16:25:58 -0300

    auteticacao admin pronta

--- Routes/administrador.js
+++ Routes/administrador.js
@@ -1,23 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const passport = require('passport');
 const usuarioController = require('../Controller/usuarioController.js');
-
-//precisa mudar e verificar se é admin
-function isAuthenticated(req, res, next) {
-    if (req.user){
-        return next();
-    }  
-    res.redirect('/login');
-}
+const autenticacao = require('../lib/autenticacaoUtil.js');
 
 // GET administrador/usuarios.
-router.get('/administrador/usuarios', isAuthenticated, function (req, res) {
+router.get('/administrador/usuarios', autenticacao.isAuthenticatedAdmin, function (req, res) {
     res.render('administrador/usuarios.ejs');
 });
 
 // GET administrador/condominio.
-router.get('/administrador/condominio', isAuthenticated, function (req, res) {
+router.get('/administrador/condominio', autenticacao.isAuthenticatedAdmin, function (req, res) {
     res.render('administrador/condominio.ejs');
 });
 
