e4b52479a3de6b064f8eb5873322d920642b7db8
thyagofernandes <thyagomau2@gmail.com>
2019-06-07 02:01:48 -0300

    salvando um servico ainda sem vincular ao usuario

--- Routes/fornecedor.js
+++ Routes/fornecedor.js
@@ -1,6 +1,27 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const router = express.Router();
 const passport = require('passport');
+const app = express();
+const pool = require('../Config/db.js');
+var tipo_servico =  [];
+// tentativa de pegar os tipos de servicos
+pool.query('SELECT nome FROM tb_tipo_servico',(err, res_bd) => {
+    if (err) {
+        console.log(err)
+      } 
+      else{
+        for(var i = 0; i<res_bd.length; i++ )
+        {     
+            tipo_servico.push(res[i]);
+        }   
+      }
+});
+
+console.log(tipo_servico,'porra arnaldo');
+//configura bodyparser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 function isAuthenticated(req, res, next) {
     if (req.user){
@@ -14,9 +35,19 @@ router.get('/fornecedor/meus-servicos', function (req, res) {
     res.render('fornecedor/meus-servicos.ejs');
 });
 
-// GET novo-servico.
+// GET novo-servico
 router.get('/fornecedor/novo-servico', function (req, res) {
     res.render('fornecedor/novo-servico.ejs');
 });
+// Cadastrando o servico no BD FALTA VINCULAR AO USUARIO
+router.post('/fornecedor/novo-servico',function(req, res){
+   pool.query('INSERT INTO tb_servico(nome,hora_abertura,abertura_status,id_tipo,banido) values ($1,current_timestamp,False,1,False)', [req.body.titulo], (err, res_bd)=> {
+    if (err) {
+      console.log(err)
+    } 
+    res.render('fornecedor/novo-servico.ejs');
+});
+    
+})
 
 module.exports = router;
