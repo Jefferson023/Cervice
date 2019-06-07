456e8518aa2cf791b06311690e9f8f2b5813093d
Jefferson Silva <jeffersonxrhudson@gmail.com>
2019-06-06 23:27:03 -0300

    Quase tudo pronto na página usuarios

--- Controller/usuarioController.js
+++ Controller/usuarioController.js
@@ -3,7 +3,7 @@ const crypt = require('../Config/crypt.js');
 
 module.exports.usuarios_condominio = function (req, res) {
     //alterar pra botar o endereco
-    query_string = 'SELECT U.nome, U.email, U.banido, C.nome AS condominio FROM'
+    query_string = 'SELECT U.nome, U.email, U.banido, C.nome AS condominio, UC.numero_casa, UC.quadra_andar FROM'
     query_string = query_string + ' tb_usuario U JOIN tb_condominio_usuario UC ON U.id_usuario = UC.id_usuario JOIN';
     query_string = query_string + ' tb_condominio C ON UC.id_condominio = C.id_condominio WHERE C.id_condominio IN';
     query_string = query_string + ' (SELECT AC.id_condominio FROM tb_usuario U2 JOIN tb_administrador_condominio AC ON';
@@ -12,6 +12,15 @@ module.exports.usuarios_condominio = function (req, res) {
         if (err){
             //internal server error page
         }
-        res.render('administrador/usuarios.ejs', {usuarios: res_bd.rows});
+        ativos = [];
+        banidos = [];
+        for (var i = 0; i < res_bd.rows.length; i++){
+            if (res_bd.rows[i].banido == false){
+                ativos.push(res_bd.rows[i])
+            }else{
+                banidos.push(res_bd.rows[i])
+            }
+        }
+        res.render('administrador/usuarios.ejs', {usuarios_ativos: ativos, usuarios_banidos: banidos});
     });
 }
